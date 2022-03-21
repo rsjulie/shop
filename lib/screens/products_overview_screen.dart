17a8aead368787b1b8ad5413480668d798d32a65
Julie <julie.ribeiro.silva@gmail.com>
2022-03-21 14:35:56 -0300

    add popup menu for filtering favorites

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import '../models/product.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = dummyPrducts;
   ProductsOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              )
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
