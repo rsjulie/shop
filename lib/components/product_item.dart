3a51edd699f785178a8d1e2a7f0700365e61c471
Julie <julie.ribeiro.silva@gmail.com>
2022-03-24 11:29:40 -0300

    add dialog confirming delete item

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -16,8 +19,43 @@ class ProductItem extends StatelessWidget {
       trailing: Container(
         width: 100,
         child: Row(children: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.PRODUCT_FORM,
+                  arguments: product,
+                );
+              },
+              icon: Icon(Icons.edit)),
+          IconButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: Text(
+                            'Tem certeza?',
+                          ),
+                          content:
+                              Text('Quer remover o item da lista de produtos?'),
+                          actions: [
+                            TextButton(
+                                child: Text('Não'),
+                                onPressed: () {
+                                  Navigator.of(context).pop(false);
+                                }),
+                            TextButton(
+                                child: Text('Sim'),
+                                onPressed: () {
+                                  Provider.of<ProductList>(
+                                    context,
+                                    listen: false,
+                                  ).removeProduct(product);
+                                  Navigator.of(context).pop(true);
+                                }),
+                          ],
+                        ));
+              },
+              icon: Icon(Icons.delete)),
         ]),
       ),
     );
